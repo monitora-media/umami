080d67590d5c9e5b31e88c33b6e77bd992e0a08d
Ewen Le Bihan <hey@ewen.works>
2024-03-31 18:56:27 +0200

    Improve empty dashboard experience

--- src/app/(main)/dashboard/DashboardPage.tsx
+++ src/app/(main)/dashboard/DashboardPage.tsx
@@ -1,21 +1,34 @@
 'use client';
-import { Icon, Icons, Loading, Text } from 'react-basics';
-import PageHeader from 'components/layout/PageHeader';
-import Pager from 'components/common/Pager';
-import WebsiteChartList from '../websites/[websiteId]/WebsiteChartList';
-import DashboardSettingsButton from 'app/(main)/dashboard/DashboardSettingsButton';
 import DashboardEdit from 'app/(main)/dashboard/DashboardEdit';
+import DashboardSettingsButton from 'app/(main)/dashboard/DashboardSettingsButton';
 import EmptyPlaceholder from 'components/common/EmptyPlaceholder';
-import { useMessages, useLocale, useTeamUrl, useWebsites } from 'components/hooks';
-import useDashboard from 'store/dashboard';
 import LinkButton from 'components/common/LinkButton';
+import Pager from 'components/common/Pager';
+import {
+  useLocale,
+  useLogin,
+  useMessages,
+  useNavigation,
+  useTeams,
+  useTeamUrl,
+  useWebsites,
+} from 'components/hooks';
+import Icons from 'components/icons';
+import PageHeader from 'components/layout/PageHeader';
+import { Button, Flexbox, Icon, Loading, Text } from 'react-basics';
+import useDashboard from 'store/dashboard';
+import WebsiteChartList from '../websites/[websiteId]/WebsiteChartList';
 
 export function DashboardPage() {
   const { formatMessage, labels, messages } = useMessages();
   const { teamId, renderTeamUrl } = useTeamUrl();
   const { showCharts, editing } = useDashboard();
+  const { router } = useNavigation();
+  const { user } = useLogin();
+  const { result: teams } = useTeams(user?.id);
   const { dir } = useLocale();
   const pageSize = 10;
+  const cloudMode = !!process.env.cloudMode;
 
   const { result, query, params, setParams } = useWebsites({ teamId }, { pageSize });
   const { page } = params;
@@ -23,6 +36,11 @@ export function DashboardPage() {
 
   const handlePageChange = (page: number) => {
     setParams({ ...params, page });
+  };
+
+  const switchTeam = (teamId: string) => {
+    const url = teamId ? `/teams/${teamId}` : '/';
+    router.push(cloudMode ? `${process.env.cloudUrl}${url}` : url);
   };
 
   if (query.isLoading) {
@@ -36,12 +54,37 @@ export function DashboardPage() {
       </PageHeader>
       {!hasData && (
         <EmptyPlaceholder message={formatMessage(messages.noWebsitesConfigured)}>
-          <LinkButton href={renderTeamUrl('/settings')}>
-            <Icon rotate={dir === 'rtl' ? 180 : 0}>
-              <Icons.ArrowRight />
-            </Icon>
-            <Text>{formatMessage(messages.goToSettings)}</Text>
-          </LinkButton>
+          <Flexbox gap={20} direction="column" alignContent="center">
+            <LinkButton href={renderTeamUrl('/settings/websites')}>
+              <Icon rotate={dir === 'rtl' ? 180 : 0}>
+                <Icons.ArrowRight />
+              </Icon>
+              <Text>{formatMessage(messages.goToSettings)}</Text>
+            </LinkButton>
+            {!teamId && teams.count > 0 ? (
+              <>
+                <Text size="md">See websites for your teams:</Text>
+                <Flexbox gap={30} justifyContent="center">
+                  {teams.count > 0 &&
+                    teams.data.map(team => (
+                      <Button key={team.id} onClick={() => switchTeam(team.id)}>
+                        <Icon>
+                          <Icons.Users></Icons.Users>
+                        </Icon>
+                        {team.name}
+                      </Button>
+                    ))}
+                </Flexbox>
+              </>
+            ) : teamId && (
+              <Button onClick={() => switchTeam('')}>
+                <Icon>
+                  <Icons.User></Icons.User>
+                </Icon>
+                Your websites
+              </Button>
+            )}
+          </Flexbox>
         </EmptyPlaceholder>
       )}
       {hasData && (
--- src/app/login/ThirdPartyProviders.tsx
+++ src/app/login/ThirdPartyProviders.tsx
@@ -1,22 +1,31 @@
 import { useEffect } from 'react';
-import styles from './ThirdPartyProviders.module.css'
-import { Button } from 'react-basics';
+import styles from './ThirdPartyProviders.module.css';
+import { Button, Flexbox } from 'react-basics';
+import LinkButton from 'components/common/LinkButton';
+import Favicon from 'components/common/Favicon';
 
-export default function ThirdPartyProviders({providers}: {providers: Array<{name: string; authorizationUrl: string}>}) {
-    useEffect(() => {
-        console.log(providers)
-    }, [providers])
-    return (
-        <div className={styles.providers}>
-            <ul className={styles.providersList}>
-                {providers.map(provider => (
-                    <li key={provider.name} className={styles.providersItem}>
-                        <Button onClick={() => {
-                            window.location.href = provider.authorizationUrl
-                        }} >{provider.name}</Button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+export default function ThirdPartyProviders({
+  providers,
+}: {
+  providers: Array<{ name: string; authorizationUrl: string }>;
+}) {
+  useEffect(() => {
+    console.log(providers);
+  }, [providers]);
+  return (
+    <div className={styles.providers}>
+      <ul className={styles.providersList}>
+        {providers.map(provider => (
+          <li key={provider.name} className={styles.providersItem}>
+            <LinkButton href={provider.authorizationUrl}>
+              <Flexbox gap={10} alignItems='center'>
+                <Favicon height={30} domain={new URL(provider.authorizationUrl).hostname} />
+                {provider.name}
+              </Flexbox>
+            </LinkButton>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
